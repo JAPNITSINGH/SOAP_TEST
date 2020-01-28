efcfac1a3daafef9d80a0be9bc40b95a179cd163
JAPNITSINGH <truejswalia@gmail.com>
2020-01-28 21:29:55 +0530

    Second local commit

--- src/com/accolite/service/ProductServiceTest.java
+++ src/com/accolite/service/ProductServiceTest.java
@@ -1,21 +1,28 @@
 package com.accolite.service;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.junit.jupiter.api.Test;
 
+
+
 class ProductServiceTest {
- private List<Product> listOfProducts = new ArrayList<Product>();
- 
+	static Logger logger = Logger.getLogger(ProductServiceTest.class.getName());
+	private List<Product> listOfProducts = new ArrayList<Product>();
+	//List<Product> p = new ArrayList<Product>;
+	 
  	void init()
  	{	listOfProducts.clear();
  		listOfProducts.add(new Product(1, "Apple"));
 		listOfProducts.add(new Product(2, "Banana"));
 		listOfProducts.add(new Product(3, "Carrot"));
 		listOfProducts.add(new Product(4, "Drum Sticks"));
+		//logger.info("inti() successful"); 
  	}
 
 	@Test
@@ -23,10 +30,13 @@ class ProductServiceTest {
 		ProductService ps = new ProductService();
 		Product expectedOutput = new Product(1, "Apple");
 		Product obtainedOutput = ps.searchByID(1);
-		if(expectedOutput.getId() == obtainedOutput.getId())
-			System.out.println("testSearchByID() success");
-		else
-			fail("testSearchByID() failed");
+//		if(expectedOutput.getId() == obtainedOutput.getId())
+//			logger.info("testSearchByID() success");//System.out.println("testSearchByID() success");
+//		else
+//			fail("testSearchByID() failed");
+		//assert expectedOutput.getId() == 2 : "testSearchByID() failed";
+		 assertTrue(obtainedOutput.getId() == expectedOutput.getId());
+		logger.info("testSearchByID() success");
 	}
 
 	@Test
@@ -60,11 +70,12 @@ class ProductServiceTest {
 		boolean expectedOutput = true;
 		Product p = new Product(1,"Apple");
 		boolean obtainedOutput = ps.deleteProduct(p);
-		if(expectedOutput && obtainedOutput)
-			System.out.println("testDeleteProduct() Success");
-		else
-			fail("testDeleteProduct() failed");
-		
+//		if(expectedOutput && obtainedOutput)
+//			logger.info("testDeleteProduct() success");//System.out.println("testDeleteProduct() Success");
+//		else
+//			fail("testDeleteProduct() failed");
+		assertTrue(obtainedOutput == expectedOutput);
+		logger.info("testDeleteProduct() success");
 	}
 
 
@@ -73,10 +84,13 @@ class ProductServiceTest {
 		ProductService ps = new ProductService();
 		boolean expectedOutput = true;
 		boolean obtainedOutput = ps.insertProduct(new Product(5,"Egg"));
-		if(expectedOutput && obtainedOutput)
-			System.out.println("testInsertProduct() success");
-		else
-			fail("testInsertProduct()");
+//		if(expectedOutput && obtainedOutput)
+//			logger.info("testInsertProduct() success");//System.out.println("testInsertProduct() success");
+//		else
+//			fail("testInsertProduct()");
+		assertTrue(obtainedOutput == expectedOutput);
+		logger.info("testInsertProduct() success");
 	}
+
 
 }
